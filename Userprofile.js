caa2bdc6d33c50bd86940a5d33b971a565fbe2f3
lesterjavillonar0721 <lesterjavillonar01@gmail.com>
2025-03-20 23:58:44 +0800

    Create Userprofile.js

---
+++ Userprofile.js
@@ -0,0 +1,14 @@
+// userProfile.js
+
+// Collecting user information using prompt()
+let fullName = prompt("Enter your full name:");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+// Logging the collected information in the console
+console.log("User Information:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
